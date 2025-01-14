4a1683f34f801d55f58bf6a2ad3e1ff2f2ca0bad
00suryavanshi00 <ssuryavanshi0001@gmail.com>
2025-01-14 13:09:29 +0530

    fast api project setup

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Ctrl T"}
+
+
+def magic_math(N: int) -> int:
+    if N < 0:
+        raise ValueError("N must be a non-negative integer.")
+    if N == 0:
+        return 0
+    if N == 1:
+        return 1
+    return magic_math(N - 1) + magic_math(N - 2) + N
+
+
+# to take inputs for different numbers
+@app.get("/magic_math/{N}")
+def calculate_magic_math(N: int):
+    try:
+        result = magic_math(N)
+        return {"N": N, "magic_math": result}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
